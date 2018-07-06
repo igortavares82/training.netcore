a2799e9c0554d69b9a0d5006b0c489d3377b1222
igortavares82 <igor.meier@gmail.com>
2018-07-05 22:34:58 -0300

    Inclusçao da interface de repositório

---
+++ Study.Training.Infrastructure/Data/Interface/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study.Training.Infrastructure.Data.Interface
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+        IEnumerable<T> Read();
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
