1a8d400e5aab6f709b1b41dbac4edd52a7b9c6c0
igortavares82 <igor.meier@gmail.com>
2018-07-16 21:59:12 -0300

    Alterações na infraestrutura

--- Study.Training.Infrastructure/Data/Context/TrainingDbContext.cs
+++ Study.Training.Infrastructure/Data/Context/TrainingDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Study.Training.Infrastructure.Mappings;
 using Study.Training.Model.Entities.Concrete;
 
@@ -9,9 +10,15 @@ namespace Study.Training.Infrastructure.Data.Context
 {
     public class TrainingDbContext : DbContext
     {
+        //https://www.learnentityframeworkcore.com/walkthroughs/console-application
+        private IConfigurationRoot config;
+
         public TrainingDbContext() { }
 
-        public TrainingDbContext(DbContextOptions<TrainingDbContext> options) : base(options) { }
+        public TrainingDbContext(IConfigurationRoot config, DbContextOptions<TrainingDbContext> options) : base(options)
+        {
+            this.config = config;
+        }
 
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Question> Quations { get; set; }
@@ -25,5 +32,12 @@ namespace Study.Training.Infrastructure.Data.Context
             QuestionMapping.Configure(builder);
             SubjectMapping.Configure(builder);
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=LUCIVAR-PC\\SQLEXPRESS;Initial Catalog=Training;Integrated Security=True");
+        }
+             
     }
 }
---
+++ Study.Training.Infrastructure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study.Training.Infrastructure
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
